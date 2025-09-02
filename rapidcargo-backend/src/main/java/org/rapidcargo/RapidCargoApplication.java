1068fb151894de1c1fe80b85345b7179b0eb2341
E-hassane <elmiloudihassane98@gmail.com>
2025-09-02 11:27:02 +0200

    feat(database): setup initial database configuration

---
+++ rapidcargo-backend/src/main/java/org/rapidcargo/RapidCargoApplication.java
@@ -0,0 +1,12 @@
+package org.rapidcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidCargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidCargoApplication.class, args);
+    }
+}
